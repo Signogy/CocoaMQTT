c6da5310f8fe0367fcf367203b1cfcf03b03f1ba
Philipp Kirchner <p.kirchner@signogy.com>
2021-09-21 12:23:26 +0200

    make it compile with xcode 13, bump to ios14, tvos14, osx10.15, swift tool 5.3

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,9 +6,9 @@ import PackageDescription
 let package = Package(
     name: "CocoaMQTT",
     platforms: [
-        .macOS(.v10_12),
-        .iOS(.v10),
-        .tvOS(.v10)
+        .macOS(.v10_15),
+        .iOS(.v14),
+        .tvOS(.v14)
     ],
     products: [
         .library(name: "CocoaMQTT", targets: [ "CocoaMQTT" ]),
